c80f13a06a649aa81940cba62e57ad0b97f065a3
Evan <evan@debuggered.com>
2018-08-31 11:53:42 -0700

    day09

---
+++ AdventOfCode/2017/js/day09/day09b.js
@@ -0,0 +1,24 @@
+const aoc = require('../lib/aoc.js');
+const instruct = aoc.inputfile('./day09.txt');
+
+let total = 0;
+let garbage = false, toggleopen = false;
+for (i = 0; i < instruct.length; i++){
+  let item = instruct[i]
+  switch(item){
+    case '!':
+      i++;
+      break;
+    case '<':
+      if(garbage) total++;
+      garbage = true;
+      break;
+    case '>':
+      garbage = false;
+      break;
+    default:
+      if(garbage) total++;
+  }
+}
+
+console.log(total)
