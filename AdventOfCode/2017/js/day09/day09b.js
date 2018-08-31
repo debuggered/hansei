7927cd99faa169c06026299a40f4ddc9292c6457
Evan <evan@debuggered.com>
2018-08-31 12:52:51 -0700

    day09

--- AdventOfCode/2017/js/day09/day09b.js
+++ AdventOfCode/2017/js/day09/day09b.js
@@ -2,7 +2,7 @@ const aoc = require('../lib/aoc.js');
 const instruct = aoc.inputfile('./day09.txt');
 
 let total = 0;
-let garbage = false, toggleopen = false;
+let garbage = false;
 for (i = 0; i < instruct.length; i++){
   let item = instruct[i]
   switch(item){
